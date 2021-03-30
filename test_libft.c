ab0ea0e7d87b2b5debcb6697d6df0adc69913733
tonTed <yddetco@gmail.com>
2021-03-29 23:27:56 -0400

    [TEST] first with ft_strlen, ft_isdigit, ft_isalpha

--- test_libft.c
+++ test_libft.c
@@ -1,9 +1,17 @@
 #include "../Unity/src/unity.h"
-#include "./src/ft_strlen.c"
 #include <string.h>
+#include <ctype.h>
+#include "./includes/header.h"
 
 char 	*s1 = "Ecole 42";
 char 	*s2 = "";
+
+void 	func_test_ascii(int (*ft)(int), int (*f)(int ))
+{
+	int i = -1;
+	while (++i <= 127)
+		TEST_ASSERT(ft(i) == f(i));
+}
 
 void	setUp(void )
 // run before each test
@@ -21,8 +29,20 @@ void	test_ft_strlen(void)
 	TEST_ASSERT(ft_strlen(s2) == strlen(s2));
 }
 
+void	test_ft_isalpha(void)
+{
+	func_test_ascii(&ft_isalpha, &isalpha);
+}
+
+void	test_ft_isdigit(void)
+{
+	func_test_ascii(&ft_isalpha, &isalpha);
+}
+
 int 	main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_ft_strlen);
+	RUN_TEST(test_ft_isalpha);
+	RUN_TEST(test_ft_isdigit);
 }
